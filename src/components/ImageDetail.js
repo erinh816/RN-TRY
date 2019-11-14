3d4f6b403bf7f7f8e5ec82a17525baf3cbc77ce4
Silu Hu <erinh816@gmail.com>
2019-11-14 15:51:26 -0500

    section 5 done

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity, Image } from 'react-native';
 
 
 
-const ImageDetail = () => {
-    return <Text>Image Detail</Text>
+const ImageDetail = (props) => {
+
+    return (
+        <View>
+            <Image source={props.imageItself} />
+            <Text>{props.imageName}</Text>
+            <Text>Image Score - ${props.imageScore}</Text>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({});
 
 export default ImageDetail;
+
+//simplified version
+
+// const ImageDetail = ({ imageItself, imageName, imageScore }) => {
+//     return (
+//         <View>
+//             <Image source={imageIteself} />
+//             <Text>{imageName}</Text>
+//             <Text>Image Score - {imageScore}</Text>
+//         </View>
+//     );
+// };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -6,12 +6,14 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail imageName='Forest' />
-            <ImageDetail imageName='Ocean' />
-            <ImageDetail imageName='River' />
+            <ImageDetail imageName='Beach' imageItself={require('../../assets/beach.jpg')} imageScore='9' />
+            <ImageDetail imageName='Forest' imageItself={require('../../assets/forest.jpg')} imageScore='7' />
+            <ImageDetail imageName='Mountain' imageItself={require('../../assets/mountain.jpg')} imageScore='4' />
         </View>
     )
 };
+
+//imageName above is prop we create
 
 const styles = StyleSheet.create({});
 
